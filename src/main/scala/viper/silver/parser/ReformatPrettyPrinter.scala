539186bd99d4184b5d09fca6a90f86bcd2ad644a
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2024-10-24 14:15:41 +0200

    Add return statements

--- src/main/scala/viper/silver/parser/ReformatPrettyPrinter.scala
+++ src/main/scala/viper/silver/parser/ReformatPrettyPrinter.scala
@@ -2,7 +2,7 @@ package viper.silver.parser
 
 import viper.silver.ast.FilePosition
 import viper.silver.ast.pretty.{Call, FastPrettyPrinterBase}
-import viper.silver.parser.PSym.{LBrace, LParen, RBrace}
+import viper.silver.parser.PSym.{Brace, LBrace, LParen, RBrace}
 import viper.silver.parser.PSymOp.RParen
 
 object ReformatPrettyPrinter extends FastPrettyPrinterBase {
@@ -26,22 +26,22 @@ object ReformatPrettyPrinter extends FastPrettyPrinterBase {
         var cont = nil
         cont = cont <> annotations.map(show).foldLeft(nil)(_ <> linebreak <> _)
         cont = cont <+> text(keyword.token) <+> text(idndef.name) <> show(args)
+        cont = cont <> returns.map(show).getOrElse(nil)
         cont
       }
-      case p@PGrouped(left: PReserved[LBrace.type], inner: Reformattable, right: PReserved[RBrace.type]) => {
-        println(s"reached first ${}, ${}", left, right);
-        show(left) <> line <> nest(defaultIndent, show(inner)) <> line <> show(right)
+      case p: PGrouped[Brace, Reformattable] if p.l.isInstanceOf[Brace] => {
+        show(p.l) <> line <> nest(defaultIndent, show(p.inner)) <> line <> show(p.r)
       }
       case p@PGrouped(left, inner: Reformattable, right)  => {
-        println("reached second");
         show(left) <> show(inner) <> show(right)
       }
       case p: PDelimited[Reformattable, Reformattable] => {
         println(s"delimited: ${p}");
         p.first.map(show).getOrElse(nil) <> p.inner.foldLeft(nil)((a, b) => a <> show(b._1) <> show(b._2)) <> p.end.map(show).getOrElse(nil)
       }
-      case p: PReserved[_] => p.display
-      case p: PIdnDef => p.display
+      case p: PMethodReturns => nil <+> show(p.k) <+> show(p.formalReturns)
+      case p: PReserved[_] => p.token
+      case p: PIdnDef => p.name
       case l: List[Reformattable] => l.map(show).reduce(_ <> _)
       case _ => text(n.reformat)
     }
